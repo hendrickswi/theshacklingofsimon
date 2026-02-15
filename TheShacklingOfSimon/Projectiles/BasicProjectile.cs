eadd6675b6ed1e061b6d2868cbd724e675d00c29
Akornss <akrem.ramadan05@gmail.com>
2026-02-15 17:10:19 -0500

    Implemented a very rudimentary projectile manager, and basic projectile weapon hooked into the player class, Needs some work though to be of complete quality but somewhat good enough for sprint2, whoever works on it next should focus on polishing + new weapons.

---
+++ TheShacklingOfSimon/Projectiles/BasicProjectile.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TheShacklingOfSimon.Sprites.Factory;
+using TheShacklingOfSimon.Sprites.Products;
+
+namespace TheShacklingOfSimon.Projectiles;
+
+public class BasicProjectile : IProjectile
+{
+	public Vector2 Position { get; private set; }
+	public Vector2 Velocity { get; set; }
+	public bool IsActive { get; private set; }
+	public Rectangle Hitbox { get; private set; }
+	public ISprite Sprite { get; set; }
+
+	public ProjectileStats Stats { get; private set; }
+
+	public BasicProjectile(Vector2 startPos, Vector2 direction, ProjectileStats stats)
+	{
+		Position = startPos;
+		Stats = stats;
+		IsActive = true;
+
+		direction.Normalize();
+		Velocity = direction * stats.Speed;
+
+		Sprite = SpriteFactory.Instance.CreateAnimatedSprite("BasicProjectile");
+
+		Hitbox = new Rectangle((int)Position.X, (int)Position.Y, 8, 8);
+	}
+
+	public void Update(GameTime gameTime)
+	{
+		float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+		Position += Velocity * dt;
+
+		Hitbox = new Rectangle((int)Position.X, (int)Position.Y, 8, 8);
+
+		Sprite?.Update(gameTime);
+	}
+
+	public void Draw(SpriteBatch spriteBatch)
+	{
+		Texture2D debugTexture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+		debugTexture.SetData(new[] { Color.White });
+
+		spriteBatch.Draw(debugTexture, Hitbox, Color.Red);
+	}
+
+	public void Discontinue()
+	{
+		IsActive = false;
+	}
+}
---
+++ TheShacklingOfSimon/Weapons/BasicWeapon.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using TheShacklingOfSimon.Projectiles;
+
+namespace TheShacklingOfSimon.Weapons;
+
+public class BasicWeapon : IWeapon
+{
+	private ProjectileManager _projectileManager;
+
+	public string Name { get; private set; }
+	public string Description { get; private set; }
+
+	public BasicWeapon(ProjectileManager manager)
+	{
+		_projectileManager = manager;
+
+		Name = "Basic Weapon";
+		Description = "Fires a simple projectile.";
+	}
+
+	public void Fire(Vector2 pos, Vector2 direction, ProjectileStats stats)
+	{
+		var projectile = new BasicProjectile(pos, direction, stats);
+		_projectileManager.AddProjectile(projectile);
+	}
+}
