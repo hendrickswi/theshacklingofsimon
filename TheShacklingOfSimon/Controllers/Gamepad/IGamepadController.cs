a4b4870a69023b5f77b1290e9b13ab85ab9c57ca
William Hendricks <whendricks6727@gmail.com>
2026-03-10 16:10:37 -0400

    Changed interface structure to avoid using a useless base interface for controllers

--- TheShacklingOfSimon/Controllers/Gamepad/IGamepadController.cs
+++ TheShacklingOfSimon/Controllers/Gamepad/IGamepadController.cs
@@ -2,5 +2,6 @@ namespace TheShacklingOfSimon.Controllers.Gamepad;
 
 public interface IGamepadController : IController<GamepadButtonInput>, IController<GamepadJoystickInput>
 {
-    
+    new void Update();
+    new void ClearCommands();
 }
--- TheShacklingOfSimon/Controllers/IController.cs
+++ TheShacklingOfSimon/Controllers/IController.cs
@@ -1,20 +1,10 @@
 ﻿namespace TheShacklingOfSimon.Controllers;
 
-/*
- * Base interface for methods with no parameters
- * (avoids ambiguity compiler issue).
- */
-public interface IController
+public interface IController<T>
 {
-    void Update();
     void ClearCommands();
+    void Update();
+    void RegisterCommand(T input, Commands.ICommand command);
+    void UnregisterCommand(T input);
 }
-
-/*
-  * Generic interface for methods that use a generic parameter.
-  */
- public interface IController<T> : IController
- {
-     void RegisterCommand(T input, Commands.ICommand command);
-     void UnregisterCommand(T input);
- }
+ 
