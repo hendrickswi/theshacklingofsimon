eee0bb4621eaa423e00b665916eae6ef84192af3
hendrickswi <whendricks6727@gmail.com>
2026-03-28 18:54:12 -0400

    Remove inventory management from player responsibility

--- TheShacklingOfSimon/Entities/Players/IPlayer.cs
+++ TheShacklingOfSimon/Entities/Players/IPlayer.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using TheShacklingOfSimon.Entities.Players.States;
 using TheShacklingOfSimon.Items;
 using TheShacklingOfSimon.Weapons;
@@ -7,36 +7,7 @@ namespace TheShacklingOfSimon.Entities.Players;
 
 public interface IPlayer : IDamageable
 {
-    /*
-     * Inherits
-     * Vector2 Position { get; }
-     * Vector2 Velocity { get; }
-     * bool IsActive { get; }
-     * Rectangle Hitbox { get; }
-     * ISprite Sprite { get; }
-     * 
-     * void Update(GameTime delta),
-     * void Draw(SpriteBatch spriteBatch),
-     * void Discontinue();
-     *
-     * To be implemented after Sprint 2:
-     * void Interact(IEntity other)
-     * 
-     * from IEntity
-     */
-    /*
-     * Inherits
-     * Health { get; },
-     * MaxHealth { get; },
-     * void TakeDamage(float amt),
-     * void Heal(float amt)
-     * from IDamageable
-     */
-    
-    Inventory Inventory { get; }
-    IWeapon CurrentPrimaryWeapon { get; }
-    IWeapon CurrentSecondaryWeapon { get; }
-    IItem CurrentItem { get; }
+    PlayerInventory Inventory { get; }
     
     // IPlayer-implementing classes will act as the context for the State pattern
     IPlayerState CurrentState { get; }
@@ -46,14 +17,6 @@ public interface IPlayer : IDamageable
     float ProjectileSpeedMultiplierStat { get; set; }
     float PrimaryAttackCooldown { get; set; }
     float SecondaryAttackCooldown { get; set; }
-    
-    void AddWeaponToInventory(IWeapon weapon);
-    IWeapon RemoveWeaponFromInventory(int pos);
-    void AddItemToInventory(IItem item);
-    IItem RemoveItemFromInventory(int pos);
-    void EquipItem(int pos);
-    void EquipPrimaryWeapon(int pos);
-    void EquipSecondaryWeapon(int pos);
     
     void RegisterMoveInput(Vector2 direction);
     void RegisterPrimaryAttackInput(Vector2 direction);
